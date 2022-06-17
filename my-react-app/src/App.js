a42994b3f8893fdc1a0c1036be472d7bcd762ff8
arianagaila <95535810+arianagaila@users.noreply.github.com>
2022-06-17 09:52:28 -0400

    Initial Commit, basic coding

---
+++ my-react-app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small> Coded by Ariana Gaila in open-source-code</small>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
